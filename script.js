6a3ff080178089a97aa109c73dc229da788a33b0
DEEP <bmwgaming97@gmail.com>
2025-02-19 18:42:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("liquidCanvas");
+[6:37 pm, 19/2/2025] Dᴇᴇᴇᴘᴊᴏᴛ 🤍: const canvas = document.getElementById("liquidCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -51,3 +51,52 @@ function animate() {
 
 init();
 animate();
+[6:41 pm, 19/2/2025] Dᴇᴇᴇᴘᴊᴏᴛ 🤍: const canvas = document.getElementById("liquidCanvas");
+if (canvas) {
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+            this.size = Math.random() * 10 + 2;
+            this.speedX = Math.random() * 2 - 1;
+            this.speedY = Math.random() * 2 - 1;
+        }
+
+        update() {
+            this.x += this.speedX;
+            this.y += this.speedY;
+            if (this.x > canvas.width || this.x < 0) this.speedX *= -1;
+            if (this.y > canvas.height || this.y < 0) this.speedY *= -1;
+        }
+
+        draw() {
+            ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
+    const particlesArray = [];
+    for (let i = 0; i < 20; i++) {
+        particlesArray.push(new Particle());
+    }
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < particlesArray.length; i++) {
+            particlesArray[i].update();
+            particlesArray[i].draw();
+        }
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+} else {
+    console.error("Canvas element not found.");
+}
