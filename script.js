32faefc70b83b992eaafa3592b1d877ddb942895
DEEP <bmwgaming97@gmail.com>
2025-02-19 19:20:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Importing Three.js library
-
 // Setting up the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
