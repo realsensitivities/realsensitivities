279f39fc1e513fd9df3d88cb57c5b9e0554836b7
DEEP <bmwgaming97@gmail.com>
2025-02-19 18:21:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
+function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(255, 255, 255, 0.2)";
+    ctx.beginPath();
+    ctx.arc(
+        Math.random() * canvas.width,
+        Math.random() * canvas.height,
+        Math.random() * 50,
+        0,
+        Math.PI * 2
+    );
+    ctx.fill();
+    requestAnimationFrame(animate);
+}
+animate()
